a01376225d4875c6fe7dbfff3b939f321c0de385
Nick Eubanks <nalexe@live.com>
2015-10-31 16:48:17 -0700

    Initial commit
    Primary functionality is implemented, launching the executable will set a pre-configured plan named 'Insomnia' as active, which should be configured to prevent the computer from going to sleep ever.
    
    Clicking the button the main window will turn off the monitor without putting the device to sleep and closing the window will restore the initial state.

---
+++ Insomnia/MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Insomnia
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        PowerPlan initialPlan = null;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            initialPlan = PowerHelpers.GetCurrentPlan();
+
+            // TODO: In order for this to work today, you must pre-configure a power plan
+            // which is named "Insomnia" and is set to make it so that the device never
+            // goes to sleep.
+            // Use the Power Management APIs to create this kind of plan on the fly based on the
+            // users current plan.
+            List<PowerPlan> plans = PowerHelpers.GetPlans();
+            for (int i = 0; i < plans.Count; ++i)
+            {
+                PowerPlan plan = plans.ElementAt(i);
+                if (plan.name.Equals("Insomnia"))
+                {
+                    PowerHelpers.SetActive(plan);
+                    break;
+                }
+            }
+        }
+
+        private void RestoreInitialPowerPlan()
+        {
+            PowerPlan currentPlan = PowerHelpers.GetCurrentPlan();
+            if (!currentPlan.Equals(initialPlan))
+            {
+                PowerHelpers.SetActive(initialPlan);
+            }
+        }
+
+        protected override void OnClosing(System.ComponentModel.CancelEventArgs e)
+        {
+            RestoreInitialPowerPlan();
+        }
+
+        private void monitorOff_Click(object sender, RoutedEventArgs e)
+        {
+            PowerHelpers.TurnOffMonitor();
+        }
+    }
+}
---
+++ Insomnia/PowerHelpers.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+
+
+namespace Insomnia
+{
+    public class PowerPlan
+    {
+        public readonly string name;
+        public Guid guid;
+
+        public PowerPlan(Guid guid)
+        {
+            this.name = PowerHelpers.GetPowerPlanFriendlyName(guid);
+            this.guid = guid;
+        }
+
+        public override bool Equals(object other)
+        {
+            PowerPlan otherPlan = (PowerPlan)other;
+            if (otherPlan == null)
+            {
+                return false;
+            }
+
+            return otherPlan.guid.Equals(this.guid);
+        }
+
+    }
+
+    class PowerHelpers
+    {
+        public static void SetActive(PowerPlan plan)
+        {
+            PowerSetActiveScheme(IntPtr.Zero, ref plan.guid);
+        }
+
+        public static List<PowerPlan> GetPlans()
+        {
+            List<PowerPlan> plans = new List<PowerPlan>();
+            List<Guid> planGuids = new List<Guid>();
+
+            IntPtr ReadBuffer;
+            uint BufferSize = 16;
+
+            uint Index = 0;
+            uint ReturnCode = 0;
+
+            while (ReturnCode == 0)
+            {
+                ReadBuffer = Marshal.AllocHGlobal((int)BufferSize);
+
+                try
+                {
+                    ReturnCode = PowerEnumerate(IntPtr.Zero, IntPtr.Zero, IntPtr.Zero, PowerDataAccessor.ACCESS_SCHEME, Index, ReadBuffer, ref BufferSize);
+
+                    if (ReturnCode == 259) break; // no more data
+                    if (ReturnCode != 0)
+                    {
+                        throw new COMException("Error occurred while enumerating power schemes. Win32 error code: " + ReturnCode);
+                    }
+
+                    Guid NewGuid = (Guid)Marshal.PtrToStructure(ReadBuffer, typeof(Guid));
+                    planGuids.Add(NewGuid);
+                }
+                finally
+                {
+                    Marshal.FreeHGlobal(ReadBuffer);
+                }
+
+                Index++;
+            }
+
+            planGuids.ForEach(delegate (Guid guid)
+            {
+                plans.Add(new PowerPlan(guid));
+            });
+
+            return plans;
+        }
+
+        private static Guid GetActiveGuid()
+        {
+            Guid activePlan = Guid.Empty;
+            IntPtr ptr = Marshal.AllocHGlobal(Marshal.SizeOf(typeof(IntPtr)));
+            if (PowerGetActiveScheme((IntPtr)null, out ptr) == 0)
+            {
+                activePlan = (Guid)Marshal.PtrToStructure(ptr, typeof(Guid));
+                if (ptr != null)
+                {
+                    Marshal.FreeHGlobal(ptr);
+                }
+            }
+            return activePlan;
+        }
+
+        public static PowerPlan GetCurrentPlan()
+        {
+            Guid guid = GetActiveGuid();
+            return GetPlans().Find(p => (p.guid == guid));
+        }
+
+        public static string GetPowerPlanFriendlyName(Guid guid)
+        {
+
+            uint buffSize = 0;
+            uint res = PowerReadFriendlyName(IntPtr.Zero, ref guid, IntPtr.Zero, IntPtr.Zero, IntPtr.Zero, ref buffSize);
+            if (res == 0)
+            {
+                if (buffSize == 0u) return "";
+                IntPtr ptrName = Marshal.AllocHGlobal((int)buffSize);
+                res = PowerReadFriendlyName(IntPtr.Zero, ref guid, IntPtr.Zero, IntPtr.Zero, ptrName, ref buffSize);
+                if (res == 0)
+                {
+                    string ret = Marshal.PtrToStringUni(ptrName);
+                    Marshal.FreeHGlobal(ptrName);
+                    return ret;
+                }
+                Marshal.FreeHGlobal(ptrName);
+            }
+
+            return String.Empty;
+        }
+
+        public static void TurnOffMonitor()
+        {
+            SendMessage(new IntPtr(HWND_TOPMOST), WM_SYSCOMMAND, new IntPtr(SC_MONITORPOWER), new IntPtr(MONITOR_OFF));
+        }
+
+        [DllImport("powrprof.dll", SetLastError = true)]
+        static extern uint PowerEnumerate(IntPtr RootPowerKey,
+                      IntPtr SchemeGuid,
+                      IntPtr SubGroupOfPowerSettingsGuid,
+                      PowerDataAccessor AccessFlags,
+                      uint Index,
+                      IntPtr Buffer,
+                      ref uint BufferSize);
+
+        enum PowerDataAccessor : uint
+        {
+            ACCESS_AC_POWER_SETTING_INDEX = 0,
+            ACCESS_DC_POWER_SETTING_INDEX = 1,
+            ACCESS_SCHEME = 16,
+            ACCESS_SUBGROUP = 17,
+            ACCESS_INDIVIDUAL_SETTING = 18,
+            ACCESS_ACTIVE_SCHEME = 19,
+            ACCESS_CREATE_SCHEME = 20
+        }
+
+        [DllImportAttribute("powrprof.dll", EntryPoint = "PowerSetActiveScheme")]
+        public static extern uint PowerSetActiveScheme(IntPtr UserPowerKey, ref Guid ActivePolicyGuid);
+
+        [DllImportAttribute("powrprof.dll", EntryPoint = "PowerGetActiveScheme")]
+        public static extern uint PowerGetActiveScheme(IntPtr UserPowerKey, out IntPtr ActivePolicyGuid);
+
+        [DllImportAttribute("powrprof.dll", EntryPoint = "PowerReadFriendlyName")]
+        public static extern uint PowerReadFriendlyName(IntPtr RootPowerKey, ref Guid SchemeGuid, IntPtr SubGroupOfPowerSettingsGuid, IntPtr PowerSettingGuid, IntPtr Buffer, ref uint BufferSize);
+
+        [DllImport("user32.dll")]
+        static extern IntPtr SendMessage(IntPtr hWnd, uint Msg, IntPtr wParam, IntPtr lParam);
+
+        static readonly int HWND_TOPMOST = -1;
+        static readonly int SC_MONITORPOWER = 0xF170;
+        static readonly uint WM_SYSCOMMAND = 0x0112;
+        static readonly int MONITOR_OFF = 2;
+    }
+}
+
